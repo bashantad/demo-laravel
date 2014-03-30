f0129a3327a9efb982c277c0d93282a316a5e43e
bashantad <dahal_bsd@yahoo.com>
2014-03-31 00:41:03 +1100

    removed unix socket from production mysql

--- app/config/database.php
+++ app/config/database.php
@@ -49,7 +49,6 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => getenv('DB_HOST'),
-			'unix_socket'   => '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock',
 			'database'  => getenv('DB_NAME'),
 			'username'  => getenv('DB_USER'),
 			'password'  => getenv('DB_PASSWORD'),
