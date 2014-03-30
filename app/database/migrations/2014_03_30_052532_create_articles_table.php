9783f07ec94f2968774a97f970964c1a16428321
bashantad <dahal_bsd@yahoo.com>
2014-03-30 16:27:37 +1100

    first commit

---
+++ app/database/migrations/2014_03_30_052532_create_articles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateArticlesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('articles', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('title');
+			$table->text('body');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('articles');
+	}
+
+}
