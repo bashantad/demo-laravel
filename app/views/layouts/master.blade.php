73c73ec5e10ec8f936b71b122b428cbe9a1f5c1c
bashantad <dahal_bsd@yahoo.com>
2014-04-06 01:03:21 +1100

    added font-awesome and jupload

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,8 +3,9 @@
 <head>
 	<title>Online resource portal</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-      <?= stylesheet_link_tag() ?>
-      {{HTML::style("vender/fontawesome/font-awesome/font-awesome.min.css")}}
+       <?= stylesheet_link_tag() ?>
+       {{ HTML::style("packages/fontawesome/css/font-awesome.min.css")}}
+
       <?= javascript_include_tag() ?>
 </head>
 <body>
